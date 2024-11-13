80baeabb53cc150f8b52ec2a3e1f82b8c996f300
Arr0w28 <smohanram28@gmail.com>
2024-11-13 22:44:07 +0530

    commit3

--- utils/database.py
+++ utils/database.py
@@ -5,8 +5,8 @@ def get_db_connection():
     connection = psycopg2.connect(
         host=DATABASE['host'],
         port=DATABASE['port'],
-        user=DATABASE['user'],
-        password=DATABASE['password'],
-        dbname=DATABASE['database']
+        user=DATABASE['mohan'],
+        password=DATABASE['Mohan2005vitcc'],
+        dbname=DATABASE['cinema-db']
     )
     return connection
