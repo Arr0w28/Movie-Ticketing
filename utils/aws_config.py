8e6170b281ecc4ba5dc6c19616450c6f3b9e2d82
Arr0w28 <smohanram28@gmail.com>
2024-11-15 12:42:22 +0530

    added username to all in db

--- utils/aws_config.py
+++ utils/aws_config.py
@@ -1,6 +1,6 @@
 DATABASE = {
     'mohan' :{
-        'host': 'movieticket.c3cwmaq8m96y.ap-south-1.rds.amazonaws.com',  # Replace with your RDS endpo
+        'host': 'movieticket.c3cwmaq8m96y.ap-south-1.rds.amazonaws.com',  # Replace with your RDS endp
         'port': 5432,  # Default PostgreSQL port
         'user': 'mohan',  # RDS username
         'password': 'mohan2005vitcc',  # RDS password
