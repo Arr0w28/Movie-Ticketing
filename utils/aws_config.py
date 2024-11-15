211784b2876538998efabe76460391fa8897f506
Arr0w28 <smohanram28@gmail.com>
2024-11-15 12:34:16 +0530

    changed password in config

--- utils/aws_config.py
+++ utils/aws_config.py
@@ -2,6 +2,6 @@ DATABASE = {
     'host': 'movieticket.c3cwmaq8m96y.ap-south-1.rds.amazonaws.com',  # Replace with your RDS endpoint
     'port': 5432,  # Default PostgreSQL port
     'user': 'mohan',  # RDS username
-    'password': 'Mohan2005vitcc',  # RDS password
+    'password': 'mohan2005vitcc',  # RDS password
     'database': 'movieticket'  # Your database name
 }
