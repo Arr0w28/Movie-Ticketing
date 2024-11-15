d829e8a6fa779943e9297d4724eba79d547c7a09
Arr0w28 <smohanram28@gmail.com>
2024-11-15 12:39:14 +0530

    added username to all in db

--- utils/database.py
+++ utils/database.py
@@ -3,10 +3,10 @@ from utils.aws_config import DATABASE
 
 def get_db_connection():
     connection = psycopg2.connect(
-        host=DATABASE['movieticket.c3cwmaq8m96y.ap-south-1.rds.amazonaws.com'],
-        port=DATABASE['5432'],
-        user=DATABASE['mohan'],
-        password=DATABASE['mohan2005vitcc'],
-        dbname=DATABASE['movieticket']
+        host=DATABASE['mohan']['movieticket.c3cwmaq8m96y.ap-south-1.rds.amazonaws.com'],
+        port=DATABASE['mohan']['5432'],
+        user=DATABASE['mohan']['mohan'],
+        password=DATABASE['mohan']['mohan2005vitcc'],
+        dbname=DATABASE['mohan']['movieticket']
     )
     return connection
