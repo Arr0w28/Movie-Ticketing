5a95abd1785b040c3b632a7c28cba4df4e7d72ee
Arr0w28 <smohanram28@gmail.com>
2024-11-15 12:17:08 +0530

    changed endpoint in conifg

--- utils/aws_config.py
+++ utils/aws_config.py
@@ -1,5 +1,5 @@
 DATABASE = {
-    'host': 'cinema-db.c3cwmaq8m96y.ap-south-1.rds.amazonaws.com',  # Replace with your RDS endpoint
+    'host': 'movieticket.c3cwmaq8m96y.ap-south-1.rds.amazonaws.com',  # Replace with your RDS endpoint
     'port': 5432,  # Default PostgreSQL port
     'user': 'mohan',  # RDS username
     'password': 'Mohan2005vitcc',  # RDS password
